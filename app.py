3cd170805a167739339b20fbc440131a196fe43b
vineet singh <tech.vineetsingh@gmail.com>
2020-10-07 19:10:12 +0530

    File Uploaded

--- app.py
+++ app.py
@@ -7,26 +7,97 @@ import tkinter as tk
 from tkinter import messagebox
 
 class cube(object):
-    rows=0
-    w=0
+    rows=200
+    w=500
     def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
-        pass
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
+
     def move(self,dirnx,dirny):
-        pass
+        self.dirnx = dirnx
+        self.dirny = dirny
+        elf.pos(self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+    
     def draw(self,surface,eyes=False):
-        pass
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+
+        pygame.draw.rect(surface, self.color, (i*dis+1, j*dis+1, dis-2, dis-2))
+        if eyes:
+            centre = dis//2
+            radius = 3
+            circleMiddle = (i*dis+centre-radius,j*dis+8)
+            circleMiddle2 = (i*dis + dis -radius*2, j*dis+8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
 
 class snake(object):
-    def __init__(self,color,pos):
-        pass
+    body = []
+    turns = {}
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+
     def move(self):
-        pass
+        for event in pygame.event.get():
+            if even.type == pygame.QUIT:
+                pygame.quit()
+            
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirn = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                if keys[pygame.K_RIGHT]:
+                    self.dirn = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                if keys[pygame.K_UP]:
+                    self.dirn = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                if keys[pygame.K_DOWN]:
+                    self.dirn = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+        
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body)-1:
+                    self.turn.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows-1)
+                else: c.move(c.dirnx, c.dirny)
+
     def reset(self,pos):
         pass
+
     def addCube(self):
         pass
-    def draw(selfself,surface):
-        pass
+
+    def draw(self,surface):
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
 
 def drawGrid(w,rows,surface):
     sizeBtwn = w // rows
@@ -41,17 +112,20 @@ def drawGrid(w,rows,surface):
 
 
 def redrawWindow(surface):
-    global rows, width
+    global rows, width, s
     surface.fill((0,0,0))
+    s.draw(surface)
     drawGrid(width, rows, surface)
     pygame.display.update()
 
 def randomSnack(rows, items):
     pass
+
 def message_box(subject,content):
     pass
+
 def main():
-    global width, rows
+    global width, rows, s
     width = 500
     height = 500
     rows = 20
@@ -67,3 +141,8 @@ def main():
         redrawWindow(win)
 
 main()
+
+
+
+
+# 1 : 15 : 00  | line 28 -36
